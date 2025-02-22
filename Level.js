48ef2ff31c50d4f52c9bb445689b9acf9bced9f1
profilenamegoeshere <jcollingreene@gmail.com>
2025-02-22 14:47:33 -0500

    walls and doors

--- Level.js
+++ Level.js
@@ -17,33 +17,52 @@ class University extends Level {
         this.rooms = [
             //third floor
             new Room(1, 3, 1), //small
-            new Wall(2, 3),
             new Room(2, 3, 1), //hallway
-            new Wall(3, 3),
             new Room(3, 3, 1), //small
-            new Wall(4, 3),
             new Room(4, 3, 1), //small
-            new Wall(5, 3),
             new Room(5, 3, 1), //medium
-            new Wall(6, 3),
+            
 
             //second floor
             new Room(2, 2, 1), //vent
             new Room(3, 2, 1), //small
-            new Wall(4, 3),
             new Room(4, 2, 1), //staircase
 
             //first floor
             new Room(0, 1, 1), //hallway spawnpoint
-            new Wall(1, 3),
             new Room(1, 1, 1), //small
-            new Wall(2, 3),
             new Room(2, 1, 1), //hallway
-            new Wall(3, 3),
             new Room(3, 1, 1), //small
-            new Wall(4, 3),
             new Room(4, 1, 2), //medium
-            new Wall(5, 3)
+
+            //third floor doors
+            new Door(2, 3),
+            new Door(3, 3),
+            new Door(4, 3),
+            new Door(5, 3),
+            new Door(6, 3),
+
+            //second floor doors
+            new Door(4, 2),
+
+            //first floor doors
+            new Door(1, 1),
+            new Door(2, 1),
+            new Door(3, 1),
+            new Door(4, 1),
+            new Door(5, 1),
+
+            //third floor walls
+            new Wall(1, 3),
+            new Wall(7, 3),
+
+            //second floor walls
+            new Wall(2, 2),
+            new Wall(5, 2),
+
+            //first floor walls
+            new Wall(0, 1),
+            new Wall(6, 1)
         ]
     }
 }
