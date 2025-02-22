4c8d40eecf504930138e01374543b95ce1e70b0e
tjnickerson <tjnickerson@vt.edu>
2025-02-21 19:59:51 -0500

    Added player movement

--- GameScreen.js
+++ GameScreen.js
@@ -9,16 +9,20 @@ class GameScreen extends GUI {
         this.camera = { x: 0, y: 0 }
     }
     Draw(x, y) {
-        this.player.update()
+        this.player.Update()
         push()
         noStroke()
         fill(255)
         rect(0, 0, 600, 400)
-        fill(0, 0, 255)
         push()
-        let targetCameraX = this.player.x + 300;
-        let targetCameraY = this.player.y + 200;
+        let targetCameraX = -this.player.x + 300;
+        let targetCameraY = -this.player.y + 200;
         translate(targetCameraX,targetCameraY)
+        fill (0)
+        rect(0,80,10,10)
+        rect(100,80,10,10)
+        rect(250,80,10,10)
+        fill(0, 0, 255)
         rect(this.player.x - 25, this.player.y - 25, 50, 50)
         pop()
         pop()
--- Player.js
+++ Player.js
@@ -2,8 +2,20 @@ class Player{
     constructor(){
         this.x = 0;
         this.y = 0;
+        this.speed = 100
     }
-    update(){
-        
+    Update(){
+        let isRight = keyIsDown(RIGHT_ARROW);
+        let isLeft = keyIsDown(LEFT_ARROW);
+        if(isLeft && isRight){
+            isLeft = false;
+            isRight = false;
+        }
+        if(isRight){
+            this.x += deltaTime / 1000 * this.speed 
+        }
+        if(isLeft){
+            this.x -= deltaTime / 1000 * this.speed 
+        }
     }
 }
