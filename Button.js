d433b7e59b7d66c3310724502bb8bcd1c4f53030
tjnickerson <tjnickerson@vt.edu>
2025-02-23 17:23:33 -0500

    Made buttons look better

--- Button.js
+++ Button.js
@@ -15,16 +15,21 @@ class Button {
     }
     //mouseX and Y
     Draw(x, y) {
-        noStroke()
+        push ()
+        textFont("Courier New")
         textSize(this.h - 10)
-        textSize(min(textSize(),textSize()*this.w/textWidth(this.text)))
+        textSize(min(this.h - 10,(this.h - 10)*.9*this.w/textWidth(this.text)))
         fill(this.color)
         if (x >= this.x && y > this.y && y <= this.y + this.h && x <= this.x + this.w) {
             fill(200)
         }
-        rect(this.x, this.y, this.w, this.h)
+        stroke(0)
+        strokeWeight(4)
+        rect(this.x, this.y, this.w, this.h,5)
+        noStroke()
         fill(0)
         textAlign(CENTER)
-        text(this.text, this.x + this.w / 2, this.y + this.h - 5)
+        text(this.text, this.x + this.w / 2, this.y + this.h *.75)
+        pop ()
     }
 }
